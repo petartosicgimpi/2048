39be583b7b4a91384e1d15214285c6581b95267f
petartosicgimpi <149800691+petartosicgimpi@users.noreply.github.com>
2024-06-19 19:41:50 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,80 +1,85 @@
-import pyautogui as pg # dozvoljava kodu da kontroliše kursor 
-import numpy as np
-import time
+import pyautogui as pg # omogućava automatizaciju kontrolisanja kursora i unosa putem tastature
+import time # pauziranje izvršavanja koda
 
 grid = []
 
-while True:
-    row = list(input('Row: '))
-    ints = []
+def unesi_podatke():
+    while True:
+        row = input('Unesite red: ')
+        if len(row) != 9 or not row.isdigit():
+            print("Nevažeći unos. Unesite tačno 9 cifara.")
+            continue
 
-    for n in row:
-        ints.append(int(n))
-    grid.append(ints)
+        ints = [int(n) for n in row]
+        grid.append(ints)
 
-    if len(grid) == 9:
-        break
-    print('Row ' + str(len(grid)) + 'Sledeći red')
+        if len(grid) == 9:
+            break
+        print('Red ' + str(len(grid)) + ' - Sledeći red')
 
-time.sleep(1)
-
-def possible(x, y, n):
-    for i in range(0, 9):
-        if grid[i][x] == n and i != y: # proverava x osu
+def moguće(x, y, n):
+    # Proveri red
+    for i in range(9):
+        if grid[y][i] == n:
             return False
 
-    for i in range(0, 9):
-        if grid[y][i] == n and i != x: # proverava y osu
+    # Proveri kolonu
+    for i in range(9):
+        if grid[i][x] == n:
             return False
 
-    x0 = (x // 3) * 3
-    y0 = (y // 3) * 3
-    for X in range(x0, x0 + 3):
-        for Y in range(y0, y0 + 3):  # proverava 3*3 kvadrate posebno
-            if grid[Y][X] == n:
-                return False    
+    # Proveri 3x3 kutiju
+    x0, y0 = (x // 3) * 3, (y // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if grid[y0 + i][x0 + j] == n:
+                return False
+
     return True
 
-def Print(matrix):
-    final = []
-    str_fin = []
-    for i in range(9):
-        final.append(matrix[i])
+def prikaži_mrežu(matrica):
+    for red in matrica:
+        print(" ".join(str(num) for num in red))
+    print()
 
-    for lists in final:
-        for num in lists:
-            str_fin.append(str(num))
+def klikni_startnu_poziciju():
+    # Pomeri miš na početnu poziciju Sudoku mreže u vašem pregledaču.
+    # Zamenite (x, y) koordinatama prve ćelije Sudoku mreže.
+    x, y = 100, 200  # Primer koordinata
+    pg.click(x, y)
 
-    counter = []
+def prikaži_rešenje(matrica):
+    str_fin = [str(num) for red in matrica for num in red]
 
-    for num in str_fin:
+    for i, num in enumerate(str_fin):
         pg.press(num)
         pg.hotkey('right')
-        counter.append(num)
-        if len(counter)%9 == 0:
+        if (i + 1) % 9 == 0:
             pg.hotkey('down')
-            pg.hotkey('left')
-            pg.hotkey('left')
-            pg.hotkey('left')
-            pg.hotkey('left')
-            pg.hotkey('left')
-            pg.hotkey('left')
-            pg.hotkey('left')
-            pg.hotkey('left')
+            for _ in range(8):
+                pg.hotkey('left')
 
-
-def solve():
+def reši():
     global grid
     for y in range(9):
         for x in range(9):
             if grid[y][x] == 0:
                 for n in range(1, 10):
-                    if possible(x, y, n):
+                    if moguće(x, y, n):
                         grid[y][x] = n
-                        solve()
+                        reši()
                         grid[y][x] = 0
                 return
-    Print(grid)
-    input("More?")
+    prikaži_rešenje(grid)
+    input("Još?")
 
-solve()
+# Glavna izvršna funkcija
+unesi_podatke()
+print("Početna Sudoku mreža:")
+prikaži_mrežu(grid)
+time.sleep(1)
+
+# Osigurajte da je pregledač aktivan i pomerite kursor na startnu poziciju
+input("Pritisnite Enter da biste započeli unos rešenja u pregledač...")
+klikni_startnu_poziciju()
+reši()
